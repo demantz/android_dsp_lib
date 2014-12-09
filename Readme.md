e14ac601d795b855b91b1e37abcf5821a85cfd1e
Dennis Mantz <dennis.mantz@googlemail.com>
2014-12-08 23:23:38 -0800

    Adding readme, changelog and licence files

---
+++ Readme.md
@@ -0,0 +1,33 @@
+DSP library for Android
+=======================
+
+This library contains DSP (digital signal processing) modules for Android 4+. 
+It uses [RenderScript](http://developer.android.com/guide/topics/renderscript/index.html).
+
+See [http://tech.mantz-it.com](http://tech.mantz-it.com) and [@dennismantz]
+(https://twitter.com/dennismantz) for updates.
+
+
+Implemented Features
+--------------------
+* Lookup table for convertion 8-bit interleaved samples to floats
+* Lookup table for downmixing 8-bit interleaved samples and converting them to floats
+* FIR filter
+* Complex FIR filter
+
+
+Testet Devices
+--------------
+
+This library is in early development right now...
+
+
+License
+-------
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+[http://www.gnu.org/licenses/gpl.html](http://www.gnu.org/licenses/gpl.html) GPL version 2 or higher
+
+principal author: Dennis Mantz <dennis.mantzgooglemail.com>
--- androiddsplibbenchmark/src/main/java/com/mantz_it/androiddsplibbenchmark/MainActivity.java
+++ androiddsplibbenchmark/src/main/java/com/mantz_it/androiddsplibbenchmark/MainActivity.java
@@ -27,6 +27,31 @@ import java.io.InputStream;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
+/**
+ * Android DSP library - Main Activity (Benchmark App)
+ *
+ * Module:      MainActivity.java
+ * Description: This is the main activity for the benchmark app
+ *
+ * @author Dennis Mantz
+ *
+ * Copyright (C) 2014 Dennis Mantz
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 public class MainActivity extends Activity implements Benchmark.BenchmarkCallback {
 	private Button bt_start;
